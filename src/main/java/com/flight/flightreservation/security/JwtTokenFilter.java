97ed5e20a32089f46e08c58c8609a13c61df4e7c
Beyza <beyzaaslan311@gmail.com>
2026-02-14 20:44:45 +0300

    add JwtTokenFilter

--- src/main/java/com/flight/flightreservation/security/JwtTokenFilter.java
+++ src/main/java/com/flight/flightreservation/security/JwtTokenFilter.java
@@ -1,5 +1,50 @@
 package com.flight.flightreservation.security;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
 
-public class JwtTokenFilter {
-    
+    private final JwtTokenUtil jwtTokenUtil;
+
+    public JwtTokenFilter(JwtTokenUtil jwtTokenUtil) {
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        String token = extractJwtFromRequest(request);
+
+        if (token != null && jwtTokenUtil.validateToken(token)) {
+            JwtUserDetails userDetails = jwtTokenUtil.getUserDetailsFromJwt(token);
+
+            if (userDetails != null && userDetails.getAuthorities().stream()
+                    .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"))) {
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null,
+                        userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+
+
+    private String extractJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // Remove "Bearer " prefix
+        }
+        return null;
+    }
 }
