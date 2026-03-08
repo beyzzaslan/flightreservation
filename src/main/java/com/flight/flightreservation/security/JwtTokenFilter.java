6a3bf0efdd1ee98600f9af00bc26d3ff14b904af
Beyza <beyzaaslan311@gmail.com>
2026-03-09 00:36:06 +0300

    jwtUserDetails

--- src/main/java/com/flight/flightreservation/security/JwtTokenFilter.java
+++ src/main/java/com/flight/flightreservation/security/JwtTokenFilter.java
@@ -11,40 +11,4 @@ import java.io.IOException;
 @Component
 public class JwtTokenFilter extends OncePerRequestFilter {
 
-    private final JwtTokenUtil jwtTokenUtil;
-
-    public JwtTokenFilter(JwtTokenUtil jwtTokenUtil) {
-        this.jwtTokenUtil = jwtTokenUtil;
-    }
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws ServletException, IOException {
-
-        String token = extractJwtFromRequest(request);
-
-        if (token != null && jwtTokenUtil.validateToken(token)) {
-            JwtUserDetails userDetails = jwtTokenUtil.getUserDetailsFromJwt(token);
-
-            if (userDetails != null && userDetails.getAuthorities().stream()
-                    .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"))) {
-
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null,
-                        userDetails.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            }
-        }
-
-        chain.doFilter(request, response);
-    }
-
-
-
-    private String extractJwtFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7); // Remove "Bearer " prefix
-        }
-        return null;
-    }
 }
