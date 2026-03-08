b4e6458bdc13b41ceccccd44f7e58f2e0a07abfe
Beyza <beyzaaslan311@gmail.com>
2026-03-09 00:36:47 +0300

    JwtUserDetails

--- src/main/java/com/flight/flightreservation/security/JwtUserDetails.java
+++ src/main/java/com/flight/flightreservation/security/JwtUserDetails.java
@@ -1,5 +1,61 @@
 package com.flight.flightreservation.security;
 
-public class JwtUserDetails {
-    
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class JwtUserDetails implements UserDetails {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private String phoneNumber;
+    private Role role;
+    // kullanıcı ile ilgili bilgileir burada topluyoruz
+
+    // şimdi constructor yazıyoruz
+    public JwtUserDetails(Long id, String email, String password, String name, String phoneNumber, Role role) {
+
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+        // kullanıcıya tek rol veriyorum ondan singletonList kullanıyorum
+
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+        // Spring Security kullanıcı doğrularken şifreyi buradan alır.
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+        // kullanıcı login olurken emailini kullanacak o yüzden username olarak emaili
+        // döndürüyoruz
+
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+
+        //Kullanıcının hesabı süresi dolmuş mu?
+    }
+
 }
