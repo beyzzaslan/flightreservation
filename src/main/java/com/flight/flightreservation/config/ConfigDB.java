1d3b348ca3d86879e79609007213a8a9c7f06438
Beyza <beyzaaslan311@gmail.com>
2026-03-10 00:17:25 +0300

    configDB

--- src/main/java/com/flight/flightreservation/config/ConfigDB.java
+++ src/main/java/com/flight/flightreservation/config/ConfigDB.java
@@ -1,4 +1,7 @@
 package com.flight.flightreservation.config;
+
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,10 +11,38 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
-@Configuration
-public class ConfigDB {
 
 
-
+    @Value("${spring.datasource.password}")
+    private String password;
     
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.potgresql.Driver");
+        dataSource.serUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        try{
+            dataSource.getConnection();
+            logger.info("Veritabanı bağlantısı başarıyla oluşturuldu.");
+
+        }
+        catch(Exception e ){
+            logger.error("Veritabanı bağlantısı oluşturuklurken bir hata oluştu : {}",e.getMessage());  
+            return dataSource;
+        }
+    }
 }
+/*
+Backend mantığıyla:
+Spring uygulamasına PostgreSQL database bağlantısını tanıtmak.
+Yani bu class:
+database driver ayarlar
+url ayarlar
+username/password ayarlar
+bağlantıyı test eder
+DataSource bean oluşturur
+
+*/
