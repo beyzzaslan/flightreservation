a1fb94bc816f445316138c3418af0cfb92c68d96
Beyza <beyzaaslan311@gmail.com>
2026-03-11 00:27:24 +0300

    DataInitializer

--- src/main/java/com/flight/flightreservation/config/DataInitializer.java
+++ src/main/java/com/flight/flightreservation/config/DataInitializer.java
@@ -1,5 +1,45 @@
 package com.flight.flightreservation.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.flight.flightreservation.model.entity.User;
+import com.flight.flightreservation.repository.UserRepository;
+
+@Configuration
 public class DataInitializer {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    CommandLineRunner init() {
+
+        return args -> {
+            User adminUser = new User();
+            adminUser.setUsername("Beyza");
+            adminUser.setEmail("beyza@gmail.com");
+            adminUser.setPhoneNumber("1234");
+            adminUser.setPassword(passwordEncoder.encode("admin123"));
+            adminUser.setRole(Role.ADMIN);
+            userRepository.save(adminUser);
+
+            System.out.println("Admin kullanıcı oluşturuldu.");
+        };
+    }
 }
+/*
+Uygulama başlar
+↓
+DataInitializer çalışır
+↓
+Admin kullanıcı oluşturulur
+↓
+Database'e kaydedilir 
+
+*/
