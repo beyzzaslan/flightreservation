38753ad12bf859f54ee382651c448cfd0a84d784
Beyza <beyzaaslan311@gmail.com>
2026-03-14 23:18:40 +0300

    bookingControllerr

--- src/main/java/com/flight/flightreservation/controller/BookingController.java
+++ src/main/java/com/flight/flightreservation/controller/BookingController.java
@@ -1,8 +1,16 @@
 package com.flight.flightreservation.controller;
-
+import com.flight.flightreservation.dto.BookingCreateDTO;
+import com.flight.flightreservation.dto.BookingDTO;
+import com.flight.flightreservation.exception.BusinessRuleException;
+import com.flight.flightreservation.exception.FlightNotFoundException;
+import com.flight.flightreservation.exception.UserNotFoundException;
+import com.flight.flightreservation.service.BookingService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
