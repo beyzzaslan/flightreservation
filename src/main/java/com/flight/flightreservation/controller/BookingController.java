4103606e4677a78e718e8219194033d8d32ed4d5
Beyza <beyzaaslan311@gmail.com>
2026-03-14 23:54:39 +0300

    solve error in bookingcontroller

--- src/main/java/com/flight/flightreservation/controller/BookingController.java
+++ src/main/java/com/flight/flightreservation/controller/BookingController.java
@@ -11,18 +11,19 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@RestController
+@RestController //json alır ve json dönderir
 @RequestMapping("/api/bookings")
 public class BookingController {
     private final BookingService bookingService;
 
     public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
+        //BookingController çalışmak için BookingService’e ihtiyaç duyar
+
     }
 
     @PostMapping("/create")
-    public ResponseEntity<BookingDTO> createBooking(@RequestBody BookingCreateDTO bookingCreateDTO) {
-
+    public ResponseEntity<BookingDTO> createBooking(@RequestBody BookingCreateDTO bookingCreateDTO) { //Client’tan gelen JSON veri, BookingCreateDTO nesnesine çevrilir.
         try {
             BookingDTO booking = bookingService.createBooking(bookingCreateDTO);
             return ResponseEntity.ok(booking);
@@ -31,19 +32,8 @@ public class BookingController {
         }
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<BookingDTO> createBooking(@RequestBody BookingCreateDTO bookingCreateDTO){
-        try{
-            BookingDTO booking = bookingService.createBooking(bookingCreateDTO);
-            return ResponseEntity.ok(booking);
-        }
-        catch(FlightNotFoundException | UserNotFoundException BusinessRuleException e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-
     @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping
+    @GetMapping //tüm rezervasyonları listelemek için kullanılır
     public ResponseEntity<List<BookingDTO>> getAllBookings() {
         List<BookingDTO> bookings = bookingService.getAllBookings();
         return ResponseEntity.ok(bookings);
